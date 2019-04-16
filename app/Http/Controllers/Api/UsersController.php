056a92158ecda40c2ff229294fc144452e024dc4
sven <1070700506@qq.com>
2019-04-16 02:58:52 +0000

    verification code

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\UsersRequest;
+
+class UsersController extends Controller
+{
+    public function store($request UsersRequest)
+    {
+
+    }
+
+
+}
